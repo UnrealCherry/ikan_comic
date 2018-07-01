4632f5ad381cbf23013b3ba126bd3deaa7da13e1
pulessrity <562514042@qq.com>
2018-07-01 15:13:12 +0800

    ip地址更换

--- static/_PubicFunction.js
+++ static/_PubicFunction.js
@@ -1,4 +1,4 @@
-var true_Url='http://23.105.194.80:60000/api/'
+var true_Url='http://67.218.132.203:60000/api/'
 var test_Url='http://192.168.1.102:60000/api/'
 export default {
   host_url:true_Url
